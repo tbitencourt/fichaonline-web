c7025eb6317796e8eab46a6cf5b860564f55e7ff
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2023-09-12 10:52:21 -0300

    :sparkles: carousel refactor

---
+++ src/components/Carousel/SliderContent.tsx
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+export interface SliderImage {
+  urls: string
+  title: string
+  description: string
+}
+
+interface SliderContentProps {
+  activeIndex: number
+}
+
+function SliderContent({ activeIndex }: SliderContentProps) {
+  const [data, setData] = useState<SliderImage[]>([])
+
+  useEffect(() => {
+    const apiUrl = 'http://localhost:3333/news'
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setData(response.data)
+      })
+      .catch((error) => {
+        console.error('Falha ao carregar os dados da API:', error)
+      })
+  }, [])
+
+  return (
+    <section>
+      {data.map((slide, index) => (
+        <div
+          key={index}
+          className={index === activeIndex ? 'slides active' : 'inactive'}
+        >
+          <img className="slide-image" src={slide.urls} alt="" />
+          <h2 className="slide-title">{slide.title}</h2>
+          <h3 className="slide-text">{slide.description}</h3>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export default SliderContent
